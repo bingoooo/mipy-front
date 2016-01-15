5ddccc0ef24d98290fd6fca042ad3f0e92ee5d7f
Dant Benjamin <sukkhato@gmail.com>
2016-01-15 17:15:40 +0100

    Modification du JS pour que la barre de navigation sélectionne la page en cours

--- public/js/load.js
+++ public/js/load.js
@@ -1,5 +1,10 @@
 'use strict';
 $(function() {
+	var pages = ["index.html?0","formdev.html?1","promotion.html?2","actualites.html?3","emploi.html?4","partenaires.html?5","profile.html?6","temoignage.html?7","team.html?8","contact.html?9"];
+	var page = window.location.search.substring(1);
+	console.log(page);
+	$('a[href="' + pages[page] + '"]').addClass('active');
+
 	$("#main_menu a").click(function(e){
 		/*e.preventDefault();
 		var fichier = $(this).attr('href');
